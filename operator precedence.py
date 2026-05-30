087c677784c5b834e025c3abf6c33f07b16d0770
all4sops2013-ui <all4sops2013@gmail.com>
2026-05-30 13:40:46 +0100

    Add files via upload

--- operator precedence.py
+++ operator precedence.py
@@ -20,3 +20,12 @@ if calc == 0:
     print("this number is divible")
 else:
     print("this number isn't divisible")
+
+
+# task 3
+
+# The mean of 40 numbers is 38. Later on, I detected that I misread the number 56 as 36. Find the correct mean of given numbers.
+
+sum = (40*38)+56-36
+mean = sum/40
+print(mean)
