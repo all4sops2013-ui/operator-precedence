b8a0ded978a63a27a5573020c15c6eafd320912a
all4sops2013-ui <all4sops2013@gmail.com>
2026-05-30 13:23:57 +0100

    Add files via upload

--- operator precedence.py
+++ operator precedence.py
@@ -7,3 +7,16 @@ print(a+c/d)
 print(c-d/a)
 print(b/d*c)
 print(d%a-b)
+
+# task 2
+
+#write to check a number is divisible by another number
+
+num1 = int(input("enter a number: "))
+num2 = int(input("enter a number: "))
+
+calc = num1 % num2
+if calc == 0:
+    print("this number is divible")
+else:
+    print("this number isn't divisible")
